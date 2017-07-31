bf18a9e3ebdd1aea07cb53be5c42e5846a4b2298
Yu Xuan Wu <ywu148@binghamton.edu>
2017-07-31 13:10:43 -0400

    Added uploading capailities

--- app/views.py
+++ app/views.py
@@ -1,4 +1,6 @@
+import os
 from flask import render_template, request
+from werkzeug import secure_filename
 from app import app
 
 @app.route('/')
@@ -8,4 +10,10 @@ def index():
                             title='Home',
     )
 
-
+@app.route('/uploader', methods = ['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['file']
+        filename = secure_filename(f.filename)
+        f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return 'file uploaded successfully'
