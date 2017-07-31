b38372ea60af71914384a38ad516e71347c48b30
Yu Xuan Wu <ywu148@binghamton.edu>
2017-07-31 11:43:25 -0400

    Test

---
+++ app/views.py
@@ -0,0 +1,11 @@
+from flask import render_template, request
+from app import app
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html',
+                            title='Home',
+    )
+
+
