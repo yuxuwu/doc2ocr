23a55a8b86ebd203a1eb33fd35d415f806d9889b
Yu Xuan Wu <ywu148@binghamton.edu>
2017-08-02 15:13:52 -0400

    Clean up code a little; can accept multiple files

--- README.md
+++ README.md
@@ -4,3 +4,12 @@ TODO:
     *Allow users to upload multiple files.
     *File conversion status.
     *Create a front-end
+
+Pipeline:
+1. Take in a batch of files
+2. For each file:
+    1. Convert to PNG format
+    2. Process the file through tesseract to a PDF file
+    3. Append file to a list of result PDFs
+3. Zip the list of PDFs into a zip file
+4. Serve zip file to user
--- app/views.py
+++ app/views.py
@@ -1,65 +1,114 @@
 import os
 import subprocess
+import zipfile
 from flask import render_template, request, send_file
 from werkzeug import secure_filename
 from app import app
 from wand.image import Image
 from PIL import Image as PI
 
-def is_pdf(filename):
-    print("Debug: Image is PDF")
-    return '.' in filename and 'pdf' in filename.rsplit('.',1)[1] 
+'''
+Defines suitable file formats
+'''
+def is_allowed(filename):
+    print("Is allowed file type")
+    return '.' in filename and filename.rsplit('.',1)[1] in set(['tif', 'tiff', 'png', 'jpg', 'jpeg', 'pdf'])
 
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.',1)[1] in set(['png', 'jpg', 'jpeg', 'pdf'])
+
+'''
+Converts image to PNG if not already PNG
+'''
+def convert_to_png(image_path):
+    png_images = []
+    with Image(filename=image_path, resolution=300) as image_png:
+        for img in image_png.sequence:
+            img_page = Image(image=img)
+            png_images.append(img_page.make_blob('png'))
+
+    return image_png
 
 
-####################################################################################
+'''
+Processes all flies in INPUT_FOLDER through Tesseract to PROCEESSED_FOLDER
+'''
+def process(temp_file, output_file):
+    command = ['tesseract', temp_file, output_file, '-l', 'eng', 'pdf']
+    proc = subprocess.Popen(command, stderr=subprocess.PIPE)
+    proc.wait()
+    
+
+
+'''
+Zips a list of files into a 'batch.zip' file.
+'''
+def zip_files(file_list):
+    zipf = zipfile.Zipfile('batch.zip', 'w')
+    for f in file_list:
+        zipf.write(f)
+    zipf.close()
+    return zipf
+
+
+'''
+Deletes all files in TEMP, INPUT, and OUPUT directories
+'''
+def cleanup():
+    os.remove(app.config['UPLOAD_FOLDER']+'/*')
+    os.remove(app.config['TEMP_FOLDER']+'/*')
+    os.remove(app.config['PROCESSED_FOLDER']+'/*')
+
+
+########################################################################################################
 
 @app.route('/')
 def index():
     return render_template('index.html',
                             title='Home',
-                            )
+                       )
 
 @app.route('/convert', methods = ['GET', 'POST'])
 def convert():
     if request.method == 'POST':
-        f = request.files['file']
-        req_images=[]
+        #Datatype of image is werkzeug.datastructures.FileStorage
+        for image in request.files.getlist('file'):
+            #Determine if file is suitable (image file type)
+            if is_allowed(image.filename):
+                #Define local name variables for file names
+                #req_images = []
+                filename = secure_filename(image.filename)
+                input_file = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+                temp_file = output_file = os.path.join(app.config['TEMP_FOLDER'], filename.rsplit('.')[0]+".png")
+                output_file = os.path.join(app.config['PROCESSED_FOLDER'], filename.rsplit('.')[0]+"-ocr")
 
-        if f and is_pdf(f.filename):
-            filename = secure_filename(f.filename)
-            input_file = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            temp_file = output_file = os.path.join(app.config['TEMP_FOLDER'], filename.rsplit('.')[0]+".png")
-            output_file = os.path.join(app.config['PROCESSED_FOLDER'], filename.rsplit('.')[0]+"-ocr")
+                print("Debug: Input_file: " + input_file)
+                print("Debug: Temp_file: " + temp_file)
+                print("Debug: Output_file: " + output_file)
 
-            print("Debug: Input_file: " + input_file)
-            print("Debug: Temp_file: " + temp_file)
-            print("Debug: Output_file: " + output_file)
+                #Saves original image file to a temporary folder
+                image.save(input_file)
 
-            f.save(input_file)
+                #Converts original image to PNG and saves it to a temporary folder to be OCRed
+                #TODO: image is closed??
+                #something = convert_to_png(input_file)
+                #save(filename=temp_file)
+                with Image(filename=input_file, resolution=300) as image_png:
+                    for img in image_png.sequence:
+                        img_page = Image(image=img)
+                        #req_images.append(img_page.make_blob('png'))
 
-            with Image(filename=input_file, resolution=300) as image_png:
-                for img in image_png.sequence:
-                    img_page = Image(image=img)
-                    req_images.append(img_page.make_blob('png'))
+                        image_png.save(filename=temp_file)
 
-                image_png.save(filename=temp_file)
+                #Process input to output
+                print("Debug: Processing " + image.filename)
+                process(temp_file, output_file)
+                print("Debug: Output_file: " + output_file+".pdf")
 
-            print("Debug: Processing " + f.filename)
-            command = ['tesseract', temp_file, output_file, '-l', 'eng', 'pdf']
-            proc = subprocess.Popen(command, stderr=subprocess.PIPE)
-            proc.wait()
+                #Clean up created files
+                print("Debug: Cleaning up")
+                cleanup()
 
-            print("Debug: Output_file: " + output_file+".pdf")
-            print("Debug: Cleaning up")
-            os.remove(input_file)
-            os.remove(temp_file)
-            os.remove(output_file)
+                try:
+                    return send_file("../"+output_file+".pdf", attachment_filename=output_file.rsplit('/')[-1])
 
-            try:
-                return send_file("../"+output_file+".pdf", attachment_filename=output_file.rsplit('/')[-1])
-
-            except Exception as err:
-                return str(err)
+                except Exception as err:
+                    return str(err)
