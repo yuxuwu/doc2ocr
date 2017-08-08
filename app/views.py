691c9e33ef3fd2e99b2441a1900e37fd625e6b75
Yu Xuan Wu <ywu148@binghamton.edu>
2017-08-08 11:16:16 -0400

    Configure file organization through YAML files

--- app/views.py
+++ app/views.py
@@ -1,20 +1,28 @@
-from app import app
 import os
 import shutil
 import glob
 import subprocess
 import zipfile
+import yaml
+import pytesseract as pyte
+from app import app
 from flask import render_template, request, send_file
 from werkzeug import secure_filename
 from wand.image import Image
 from PIL import Image as IM
-import pytesseract as pyte
+
+'''
+Loads yaml config file into dictionary container
+'''
+def load_config(config_file):
+    config_map = yaml.safe_load(config_file.read())
+    print(config_map)
+
 
 '''
 Defines suitable file formats
 '''
 def is_allowed(filename):
-    print("Is allowed file type")
     return '.' in filename and filename.rsplit('.',1)[1] in set(['tif', 'tiff', 'png', 'jpg', 'jpeg', 'pdf'])
 
 
@@ -39,14 +47,28 @@ def convert_to_string(png_image):
 
 
 '''
-Moves file to a specified folder. Creates folder if folder is not present
-Parameters: input_file - file to be moved
-            dest_folder - directory file is moved to 
+Moves file to specified folder
 '''
-def send_to_processed(input_file, dest_folder):
+def move_file(input_file, dest_folder):
     if not os.path.isdir(dest_folder):
         os.mkdir(dest_folder)
         shutil.move(input_file, dest_folder)
+
+
+'''
+Moves file to a specified folder. Creates folder if folder is not present
+Parameters: input_file - file to be moved
+            image_string - string contents of the file
+'''
+def send_to_processed(input_file, image_string):
+    to_be_defaulted = True
+    for folder in folder_config.keys():
+        for keyword in folder_config[folder]:
+            if keyword in image_string:
+                to_be_defaulted = False
+                dest_folder = app.config['PROCESSED_FOLDER']+folder
+                move_file(input_file, dest_folder)
+                return to_be_defaulted
 
 
 '''
@@ -81,7 +103,7 @@ def cleanup():
     #for file in glob.glob(app.config['ZIP_DOWNLOAD']+'*'):
     #    os.remove(file)
 
-test_folder_config = {u"COMPLIANT": "Page",u"TESTS": "Last"}
+folder_config = {u"COMPLIANT": ["Page"],u"TESTS": ["Last"]}
 
 
 ########################################################################################################
@@ -95,6 +117,8 @@ def index():
 @app.route('/convert', methods = ['GET', 'POST'])
 def convert():
     if request.method == 'POST':
+        #Load yaml config file
+        load_config(request.files['config'])
         #Datatype of image is werkzeug.datastructures.FileStorage
         for image in request.files.getlist('file'):
             #Determine if file is suitable (image file type)
@@ -109,26 +133,19 @@ def convert():
                 #Saves original image file to a temporary folder
                 image.save(input_file)
 
-                #Converts original image to PNG, then to string
+                #Converts original image to PNG, then to string from its contents
                 convert_to_png(input_file, temp_file)
                 image_string = convert_to_string(temp_file).upper()
-                to_be_defaulted = True
-                #temp_string_list = image_string.rsplit(' ')
-                for key in test_folder_config.keys():
-                    print(key)
-                    if key in image_string:
-                        to_be_defaulted = False
-                        dest_folder = app.config['PROCESSED_FOLDER']+test_folder_config[key]
-                        send_to_processed(input_file, dest_folder)
-                        break
+                #Sends orig. pdf to folder based on string contents
+                to_be_defaulted = send_to_processed(input_file, image_string)
                 
                 if to_be_defaulted:
-                    send_to_processed(input_file, app.config['DEFAULT_FOLDER'])                    
+                    move_file(input_file, app.config['DEFAULT_FOLDER'])                    
 
         zipf = zip_files(app.config['PROCESSED_FOLDER'])
 
         #Clean up created files
-        #cleanup()
+        cleanup()
 
         try:
             zip_path = '../'+app.config['ZIP_DOWNLOAD']+'batch.zip'
